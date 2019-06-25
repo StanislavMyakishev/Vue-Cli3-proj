69d7ec3d8dce4ab6f14f6372be54d8a0f55b1901
StanislavMyakishev <masiff-s@live.ru>
2019-06-26 01:21:54 +0300

    new organizations page

--- src/components/Items/Order.vue
+++ src/components/Items/Order.vue
@@ -1,23 +1,96 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-container>
-        <v-layout row>
+        <v-layout align-center justify-center column fill-height>
             <v-flex xs12>
-                <v-card>
-                    <v-img
-                            :src="testOrder.imageSrc"
-                            height="300px"
+                <v-card
+                        class="elevation-10 mb-3">
+                    <v-responsive
+                            class="rel"
                     >
-                    </v-img>
+                        <img class="abs" src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+                        <v-img
+                                :src="testOrder.imageSrc"
+                                height="300px"
+                        >
+                        </v-img>
+                    </v-responsive>
                     <v-card-text>
-                        <h1 class="text--primary">{{testOrder.title}} # {{ id }}</h1>
-                        <h2>{{testOrder.company}}</h2>
-                        <p>{{testOrder.description}}</p>
+                        <h1 class="text-md-center">{{testOrder.title}}</h1>
+                        <h2 class="text-md-center"> от компании "{{testOrder.company}}"</h2>
+                        <p class="text-md-center">{{testOrder.description}}</p>
+                        <p class="text-md-right">{{testOrder.createdDate.getFullYear() + '/' +
+                            testOrder.createdDate.getMonth() + '/' + testOrder.createdDate.getDate() + ' ' +
+                            testOrder.createdDate.getHours() + ':' + testOrder.createdDate.getMinutes()}} </p>
                     </v-card-text>
+                </v-card>
+                <v-card v-if=perform>
+                    <v-card-text>
+                        <v-textarea
+                                label="Комментарий">
+                        </v-textarea>
+                    </v-card-text>
+                    <div class="text-xs-center">
+                        <v-btn large color="primary">Оставить заявку на заказ</v-btn>
+                    </div>
+                </v-card>
+                <v-card v-else>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn class="warning" flat>Edit</v-btn>
-                        <v-btn class="success">Get</v-btn>
+                        <div class="text-xs-center">
+                            <v-btn large color="success">Заказ выполнен</v-btn>
+                        </div>
+                        <div class="text-xs-center">
+                            <v-btn large color="warning">Изменить заказ</v-btn>
+                        </div>
+                        <div class="text-xs-center">
+                            <v-btn large color="error">Удалить заказ</v-btn>
+                        </div>
                     </v-card-actions>
+                </v-card>
+                <v-card v-if=!perform>
+                    <v-list two-line>
+                        <template v-for="(item, index) in performers">
+                            <v-subheader
+                                    v-if="item.header"
+                                    :key="item.header"
+                            >
+                                {{ item.header }}
+                            </v-subheader>
+
+                            <v-divider
+                                    v-else-if="item.divider"
+                                    :key="index"
+                                    :inset="item.inset"
+                            ></v-divider>
+
+                            <v-list-tile
+                                    v-else
+                                    :key="item.title"
+                                    avatar
+                            >
+                                <v-list-tile-avatar>
+                                    <img :src="item.avatar">
+                                </v-list-tile-avatar>
+
+                                <v-list-tile-content>
+                                    <v-list-tile-title v-html="item.performerName"></v-list-tile-title>
+                                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                                </v-list-tile-content>
+                                <v-btn
+                                        v-if=!hired
+                                        color="success"
+                                        @click="hired = !hired"
+                                >Принять заявку
+                                </v-btn>
+                                <v-btn
+                                        v-else
+                                        color="error"
+                                        @click="hired = !hired"
+                                >Удалить перформера
+                                </v-btn>
+                            </v-list-tile>
+                        </template>
+                    </v-list>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -28,15 +101,57 @@
     export default {
         data() {
             return {
+                perform: false,
+                hired: false,
                 id: this.$route.params.id,
                 testOrder: {
-                    title: 'Test order',
-                    company: 'Squirrel LTD',
-                    description: 'This is test order page, just look at this qt pie',
+                    title: 'Фриланс-биржа',
+                    company: 'Университет ИТМО',
+                    category: "IT",
+                    createdDate: new Date(2019, 0, 1, 0, 0, 0, 0),
+                    description: 'Наша компания предлагает Вам выполнить восхитительный проект: создание фриланс биржи!' +
+                        ' Нам необходима реализация сайта этого проекта на стеке python + js используя vue (использование ' +
+                        'инструментов по типу vuetify оставется на ваш вкус). Сайт должен быть красивым и функциональным,' +
+                        'страницы должны быть реализованы с использованием material design компонентов и в принципе выглядеть ' +
+                        'приятно. Это сампл описания заказа, так что особо не зачитывайтесь. Далее будет сгенерированный текст,' +
+                        ' спасибо за внимание! ' + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                     imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-                    id: '1'
-                }
+                    id: '1',
+                },
+                performers: [
+                    {header: 'Комментарии'},
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                        performerName: 'Иван Иванов',
+                        subtitle: "<span class='text--primary'>Проект интересный, очень заинтересован его выполнить за зачет по вебу :)</span>"
+                    },
+                    {divider: true, inset: true},
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                        performerName: 'Павел Григорьев <span class="grey--text text--lighten-1"></span>',
+                        subtitle: "<span class='text--primary'>Куда нажать, чтобы просто получить зачет по вебу?</span>"
+                    },
+                    {divider: true, inset: true},
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                        performerName: 'Анна Некифорова',
+                        subtitle: "<span class='text--primary'>Согласна остановить горящую избу на скаку за тройку по вебу</span>"
+                    }
+                ]
             }
         }
     }
 </script>
+
+<style>
+    .rel {
+        position: relative;
+    }
+
+    .abs {
+        top: 20px;
+        left: 20px;
+        position: absolute;
+        z-index: 100;
+    }
+</style>
--- src/components/User/OrganizationInfo.vue
+++ src/components/User/OrganizationInfo.vue
@@ -1,56 +1,196 @@
 <template>
-    <v-content>
-        <v-container fluid fill-height>
-            <v-layout align-center justify-center row>
-                <v-flex xs12 sm12 md10>
-                    <v-card>
-                        <v-toolbar dark color="primary">
-                            {{organization.name}}
-                        </v-toolbar>
-                        <v-card-title>
-                            TIN: {{organization.itn}}
-                        </v-card-title>
-                        <v-divider></v-divider>
-                        <v-card-title>
-                            {{organization.description}}
-                        </v-card-title>
-                        <v-divider></v-divider>
-                        <v-card-title>
-                            Website: {{organization.website}}
-                        </v-card-title>
-                        <v-divider></v-divider>
-                        <v-card-title>
-                            {{organization.address}}
-                        </v-card-title>
-                    </v-card>
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </v-content>
+    <v-container grid-list-md text-xs-center>
+        <v-layout>
+            <v-flex>
+                <v-card>
+                    <v-layout
+                            row wrap>
+                        <v-flex xs4 md4 sm4>
+                            <v-card flat>
+                                <v-responsive>
+                                    <v-avatar
+                                            :tile="tile"
+                                            :size="300"
+                                            color="grey lighten-4"
+                                    >
+                                        <img src="https://vuetifyjs.com/apple-touch-icon-180x180.png" alt="avatar">
+                                    </v-avatar>
+                                </v-responsive>
+                                <v-card-text>
+                                    <h2 class="text-md-center">{{organization.address}}</h2>
+                                    <h2 class="text-md-center">{{organization.website}}</h2>
+                                    <h2 class="text-md-center">ИНН: {{organization.itn}}</h2>
+                                </v-card-text>
+                                <v-card-actions class="pa-3">
+                                    Рейтинг:
+                                    <v-spacer></v-spacer>
+                                    <span class="grey--text text--lighten-2 caption mr-2">({{ rating }})</span>
+                                    <v-rating
+                                            v-model="rating"
+                                            background-color="white"
+                                            color="yellow accent-4"
+                                            dense
+                                            half-increments
+                                            hover
+                                            size="18"
+                                    ></v-rating>
+                                </v-card-actions>
+                            </v-card>
+                        </v-flex>
+                        <v-flex xs8 md8 sm8>
+                            <v-card flat>
+                                <v-card-text class="headline">
+                                    <span class="text-md-center">{{organization.name}}</span>
+                                </v-card-text>
+                                <v-card-text class="text-sm-left">
+                                    <span class="text--primary">{{organization.description}}</span>
+                                </v-card-text>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout>
+                        <v-flex xs12 md12 sm12>
+                            <v-card flat>
+                                <v-list two-line>
+                                    <template v-for="(user, index) in users">
+                                        <v-subheader
+                                                v-if="user.header"
+                                                :key="user.header"
+                                        >
+                                            {{ user.header }}
+                                        </v-subheader>
+
+                                        <v-divider
+                                                v-else-if="user.divider"
+                                                :key="index"
+                                                :inset="user.inset"
+                                        ></v-divider>
+
+                                        <v-list-tile
+                                                v-else
+                                                :key="user.title"
+                                                avatar
+                                        >
+                                            <v-list-tile-avatar>
+                                                <img :src="user.avatar">
+                                            </v-list-tile-avatar>
+
+                                            <v-list-tile-content>
+                                                <v-list-tile-title v-html="user.performerName"></v-list-tile-title>
+                                                <v-list-tile-sub-title v-html="user.subtitle"></v-list-tile-sub-title>
+                                            </v-list-tile-content>
+                                            <v-card-actions class="pa-3">
+                                                Оценка:
+                                                <v-spacer></v-spacer>
+                                                <span class="grey--text text--lighten-2 caption mr-2">({{ user.rating }})</span>
+                                                <v-rating
+                                                        v-model="user.rating"
+                                                        background-color="white"
+                                                        color="yellow accent-4"
+                                                        dense
+                                                        :readonly="true"
+                                                        half-increments
+                                                        hover
+                                                        size="18"
+                                                ></v-rating>
+                                            </v-card-actions>
+                                        </v-list-tile>
+                                    </template>
+                                </v-list>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout>
+                        <v-flex xs12 md12 sm12>
+                            <v-card flat v-if=perform>
+                                <v-card-text>
+                                    <v-textarea
+                                            label="Комментарий">
+                                    </v-textarea>
+                                </v-card-text>
+                                <v-card-actions class="pa-3">
+                                    Оценка:
+                                    <v-spacer></v-spacer>
+                                    <span class="grey--text text--lighten-2 caption mr-2">({{ ownRating }})</span>
+                                    <v-rating
+                                            v-model="ownRating"
+                                            background-color="white"
+                                            color="yellow accent-4"
+                                            half-increments
+                                            hover
+                                            size="18"
+                                    ></v-rating>
+                                </v-card-actions>
+                                <div class="text-xs-center">
+                                    <v-btn large color="primary">Отправить отзыв</v-btn>
+                                </div>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-    import axios from 'axios'
 
     export default {
         props: ['id'],
         data() {
             return {
-                organization: []
+                perform: true,
+                rating: 2,
+                ownRating: 5,
+                organization: {
+                    name: 'Университет ИТМО',
+                    itn: '12345678912',
+                    description: 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa',
+                    website: 'www.itmo.ru',
+                    imgSrc: 'https://vuetifyjs.com/apple-touch-icon-180x180.png',
+                    address: 'Ulitsa Pushkina dom Kolotushkina',
+                },
+                users: [
+                    {header: 'Отзывы'},
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+                        performerName: 'Иван Иванов',
+                        subtitle: "<span class='text--primary'>Организация неплохая и выпускники интересные!</span>",
+                        rating: 5,
+                    },
+                    {divider: true, inset: true},
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                        performerName: 'Павел Григорьев <span class="grey--text text--lighten-1"></span>',
+                        subtitle: "<span class='text--primary'>Куда нажать, чтобы просто получить зачет по вебу в этой контрое?</span>",
+                        rating: 4,
+                    },
+                    {divider: true, inset: true},
+                    {
+                        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                        performerName: 'Анна Некифорова',
+                        subtitle: "<span class='text--primary'>Какая оценка по вебу у меня, такая будет и у Вас!!!!</span>",
+                        rating: 3,
+                    }
+                ],
             }
         },
-        created() {
-            // const organization_id = this.$route.organization.id;
-            // axios.get('http://127.0.0.1:8081/api/organizations/', organization_id)
-            axios.get('http://127.0.0.1:8081/api/organizations/2/')
-                .then(response => response.data)
-                .then(data => {
-                    this.organization = data;
-                })
-                .catch(error => {
-                    console.log(error)
-                });
-        }
-        //    ДОБАВИТЬ ОТЗЫВЫ
+        methods: {}
     }
 </script>
+
+<style>
+    .float {
+        float: left;
+        text-align: center;
+    }
+
+    .abs {
+        height: 180px;
+        width: 180px;
+        top: 20px;
+        left: 20px;
+        position: absolute;
+        z-index: 100;
+    }
+</style>
