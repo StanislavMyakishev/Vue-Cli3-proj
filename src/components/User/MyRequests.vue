64788b57d45aaf4ce5a35c87d2de993db1cb3d20
valkmorgenstern <anastasiavalka@gmail.com>
2019-06-27 00:24:24 +0300

    MyRequests changed, need to check

--- src/components/User/MyRequests.vue
+++ src/components/User/MyRequests.vue
@@ -5,31 +5,33 @@
                 <v-card flat>
                     <v-card-text
                             class="display-2">
-                        <span class="clr">My requests</span>
+                        <span class="clr">Отправлены заявки на заказы:</span>
                     </v-card-text>
                 </v-card>
                 <v-card
                         class="elevation-10 mb-3"
-                        v-for="ord in ordList"
+                        v-for="ord in requestedOrders"
                         :key="ord.id">
                     <v-layout row>
                         <v-flex xs4>
                             <v-img
-                                    :src="ord.imageSrc"
-                                    height="160px">
+                                    :src="'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'"
+                                    height="230px">
                             </v-img>
                         </v-flex>
                         <v-flex xs8>
                             <v-card-text>
-                                <h2 class="text--primary">{{ord.title}}</h2>
+                                <h2 class="text--primary">{{ord.name}}</h2>
+                                <p>{{ord.customer.name}}</p>
                                 <p>{{ord.description}}</p>
+                                <p>{{ord.date_created | parseDate}}</p>
                             </v-card-text>
                             <v-card-actions>
                                 <v-spacer></v-spacer>
                                 <v-btn
                                         class="secondary"
-                                        :to="/ord/ + ord.id">
-                                    Open
+                                        :to="'/order/?id=' + ord.id">
+                                    Открыть
                                 </v-btn>
                             </v-card-actions>
                         </v-flex>
@@ -41,25 +43,31 @@
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         data() {
             return {
-                ordList: [
-                    {
-                        title: 'First order',
-                        description: 'Order description',
-                        imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-                        id: '1'
-                    },
-                    {
-                        title: 'Second order',
-                        description: 'Second but not the last',
-                        imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-                        id: '2'
-                    }
-                ]
+                requestedOrders: [],
+                imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
             }
-        }
+        },
+        mounted() {
+            this.getMyRequests();
+            this.$root.$on('updateRequests', this.getMyRequests);
+        },
+        methods: {
+            getMyRequests() {
+                let config = this.config;
+                axios.get('http://127.0.0.1:8081/api/organizations/' + localStorage.getItem('userId') + '/get_requested_orders/', config)
+                    .then(response => response.data)
+                    .then(data => {
+                        this.requestedOrders = data;
+                    })
+                    .catch(error => console.log(error));
+            }
+        },
+        props: ['config']
     }
 </script>
 
